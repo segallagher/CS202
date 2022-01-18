7db68c228896f4b89fbe55b2680af6ea30f2c438
Samuel Gallagher <segallagher@alaska.edu>
2022-01-18 14:48:20 -0900

    Changed temperature.cpp to use console commands

--- temperature/temperature.cpp
+++ temperature/temperature.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include <string>
 
 double cpp_ftoc(const char* str) {
 	char* temp;
@@ -13,7 +13,18 @@ double c_ctof(const char* str) {
 	return celsius * 9 / 5 + 32;
 }
 
-void main(int argc, const char** argv) {
-	std::cout << cpp_ftoc("35") << std::endl;
-	std::cout << c_ctof("35") << std::endl;
+void main(int argc,char *argv[]) {
+
+	if (argc < 1) {
+		std::cout << "Arguments needed" << std::endl;
+	}
+	else if (std::string(argv[1]) == "--ftoc") {
+		std::cout << cpp_ftoc(argv[2]) << std::endl;
+	}
+	else if (std::string(argv[1]) == "--ctof") {
+		std::cout << c_ctof(argv[2]) << std::endl;
+	}
+	else {
+		std::cout << "Unknown error" << std::endl;
+	}
 }
