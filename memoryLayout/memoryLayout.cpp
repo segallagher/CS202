422f6e8dedb40fb4ea0677606b229453a8d72a0b
Samuel Gallagher <segallagher@alaska.edu>
2022-02-19 15:51:18 -0900

    made printPointer() print out an integer location for the memory address

--- memoryLayout/memoryLayout.cpp
+++ memoryLayout/memoryLayout.cpp
@@ -1,12 +1,22 @@
 #include <iostream>
 
-void printPointer(int i) {
+static int glob = 100;
+
+void printPointer(int & i) {
     int* iptr = &i;
+    int mIntAddress = (int)&i/sizeof(int); //memory location of integer variable in the form of an integer
     std::cout << "i = " << *iptr << "\n";
-    std::cout << "&i = " << iptr << "\n";
+    //std::cout << "&i = " << iptr << "\n";
+    std::cout << "&i = " << mIntAddress << "\n";
 }
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    printPointer(glob);
+    std::cout << "Array addresses" << std::endl;
+    int joe[] = { 2,5,6,7 };
+    for (int i = 0; i < 4; i++) {
+        printPointer(joe[i]);
+        std::cout << "-----------------" << std::endl;
+    }
 }
