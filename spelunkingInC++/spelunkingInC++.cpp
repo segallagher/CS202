027f1ee001974c6b4fdefea722629962d2b074f7
Samuel Gallagher <segallagher@alaska.edu>
2022-02-22 14:16:38 -0900

    designed cave system

--- spelunkingInC++/spelunkingInC++.cpp
+++ spelunkingInC++/spelunkingInC++.cpp
@@ -45,19 +45,34 @@ int main()
 {
 	string previousLocation = "cave river";
 	string currentLocation = "damp room";
-	//CaveJunction names must be all lower case
+	//CaveJunction names must be all lower case, last connection slot of CaveJunction() must be filled
 	map<string, CaveJunction> caveSystemBase{
-		std::pair<string,CaveJunction>("damp room",CaveJunction("cave river","","4")),
-		std::pair<string,CaveJunction>("cave river",CaveJunction("damp room","3","4")),
-		std::pair<string,CaveJunction>("3",CaveJunction("","cave river","4")),
-		std::pair<string,CaveJunction>("4",CaveJunction("damp room","3","cave river")),
+		std::pair<string,CaveJunction>("damp room",CaveJunction("cave river","","large cavern")),
+		std::pair<string,CaveJunction>("cave river",CaveJunction("damp room","fast flowing water","river head")),
+		std::pair<string,CaveJunction>("river head",CaveJunction("","cave river","mossy chamber")),
+		std::pair<string,CaveJunction>("mossy chamber",CaveJunction("","","river head")),
+		std::pair<string,CaveJunction>("fast flowing water",CaveJunction("cave river","large cavern","waterfall")),
+		std::pair<string,CaveJunction>("waterfall",CaveJunction("","","fast flowing water")),
+		std::pair<string,CaveJunction>("large cavern",CaveJunction("damp room","bat roost","fast flowing water")),
+		std::pair<string,CaveJunction>("bat roost",CaveJunction("large cavern","tight passage","large slope")),
+		std::pair<string,CaveJunction>("tight passage",CaveJunction("old campfire","large slope","bat roost")),
+		std::pair<string,CaveJunction>("old campfire",CaveJunction("tight passage","","root filled passage")),
+		std::pair<string,CaveJunction>("root filled passage",CaveJunction("old campfire","windy room","wobbling stalagtites")),
+		std::pair<string,CaveJunction>("large slope",CaveJunction("bat roost","tight passage","wobbling stalagtites")),
+		std::pair<string,CaveJunction>("wobbling stalagtites",CaveJunction("large slope","root filled passage","windy room")),
+		std::pair<string,CaveJunction>("windy room",CaveJunction("wobbling stalagtites","root filled passage","cave opening")),
+		std::pair<string,CaveJunction>("cave opening",CaveJunction("windy room","","outside")),
+		std::pair<string,CaveJunction>("outside",CaveJunction("","","cave opening")),
 	};
 	CaveSystem caveSystem(caveSystemBase,currentLocation,previousLocation);
-	std::cout << caveSystem << std::endl;
+	std::cout << "Cave created" << std::endl;
 	writeCaveFile(caveSystem, "joe.txt");
+	std::cout << "Cave Written" << std::endl;
 	map<string,CaveJunction> a = readCaveFile("joe.txt");
+	std::cout << "Cave read" << std::endl;
 	CaveSystem caveSystem2(a,currentLocation,previousLocation);
-	std::cout << caveSystem2 << std::endl;
+	std::cout << "Cave system created" << std::endl;
+	std::cout << "Starting Game" << std::endl << std::endl;
 	caveSystem2.move();
 	caveSystem2.move();
 	caveSystem2.move();
