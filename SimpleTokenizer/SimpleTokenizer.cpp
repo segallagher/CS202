06e741c63aaa6bd341b05d87fc7d76aaf3abb0f3
Samuel Gallagher <segallagher@alaska.edu>
2022-03-04 12:48:54 -0900

    added file opening and beginning custom tokenizer to keep track of column information

--- SimpleTokenizer/SimpleTokenizer.cpp
+++ SimpleTokenizer/SimpleTokenizer.cpp
@@ -1,4 +1,27 @@
 #include <iostream>
+using std::cout;
+using std::endl;
+#include "Word.h"
+#include <vector>
+using std::vector;
+#include <fstream>
+using std::ifstream;
+using std::ofstream;
+
+vector<Word> createWordData(string & fileName) {
+    ifstream file(fileName);
+    if (!file.is_open()) {
+        cout << "Error reading file " << fileName << endl;
+        return vector<Word>{};
+    }
+    vector<Word> words;
+    string line;
+    while (std::getline(file, line)) {
+        for (auto n : line) {
+            /////////////////////////////
+        }
+    }
+}
 
 int main()
 {
