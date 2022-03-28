526772f01c7612a3db44035c04706547e0364bba
Samuel Gallagher <segallagher@alaska.edu>
2022-03-28 00:29:56 -0800

    added printColumns(),generatePaper(),appendPaper(), completely changed twocolumn()

--- Question1/PaperColumn.hpp
+++ Question1/PaperColumn.hpp
@@ -1,12 +1,14 @@
 #pragma once
 #include <vector>
 #include <string>
+#include <iostream>
 
 class PaperColumn
 {
 public:
 	PaperColumn() {};
 	PaperColumn(std::vector<std::string>& lines) { _lines = lines; }
+	void appendLine(const std::string& str) { _lines.push_back(str); }
 	std::string returnLine(int& i) { return _lines.at(i); }
 private:
 	std::vector<std::string> _lines;
--- Question1/prettyprint.cpp
+++ Question1/prettyprint.cpp
@@ -8,6 +8,8 @@ using std::ifstream;
 #include <sstream>
 using std::istringstream;
 using std::ostringstream;
+#include <iomanip>
+using std::setw;
 
 #include "Tokenizer.hpp"
 #include "PaperColumn.hpp"
@@ -78,25 +80,115 @@ string prettyPrint(vector<string> & paragraphs,const int & spacing) {
 	return output.str();
 }
 
-void twocolumn(vector<string>& paragraphs,const int & lines, const int& spacing, const int & margin) {
-	vector<PaperColumn> paper;
+void printColumns(vector<PaperColumn>& papers,const int &columns, const int& lines, const int& spacing, const int& margin) {
+#if 0
+	int totalPapers = 0;
+	for (int b = 0; b < papers.size(); b++) {
+		std::ostringstream out;
+		for (int k = 0; k < lines; k++) {
+			for (int i = 0; i < columns; i++) {
+				out << setw(spacing) << std::left << papers.at(i + totalPapers).returnLine(k);
+				if (i < columns - 1) {
+					for (int space = 0; space < margin; space++) {
+						out << " ";
+					}
+				}
+				totalPapers++;
+			}
+			out << '\n';
+		}
+		for (int h = 0; h < columns * (spacing + margin) - margin; h++) { out << "-"; }
+		std::cout << out.str() << std::endl;
+		out.str("");
+	}
+#endif
+	ostringstream out;
+	for (int i = 0; i < papers.size(); i+= columns) { //sheets
+		for (int k = 0; k < lines; k++) {//lines
+			for (int q = 0; q < columns; q++) {//columns
+				out << setw(spacing) << std::left << papers.at(i + q).returnLine(k);
+				if (q < columns-1) {
+					for (int s = 0; s < margin; s++) {
+						out << "|";
+					}
+				}
+			}
+			out << '\n';
+		}
+		out << "------------------------------------------------------------\n";
+	}
+	std::cout << out.str() << std::endl;
+}
+
+string generatePaper(vector<string>& bookLines, const int& lines, const int& spacing, const int& margin) {
+	ostringstream out;
+	for (int g = 0; g < lines; g++) {
+		out << *bookLines.begin() << '\n';
+		bookLines.erase(bookLines.begin());
+	}
+
+	return out.str();
+}
+
+void appendPaper(vector<PaperColumn>& papers, string& paper) {
+	vector<string> ba = Tokenize(paper, '\n');
+	PaperColumn a(ba);
+	papers.push_back(a);
+}
+
+void twocolumn(vector<string>& paragraphs, const int& lines, const int& spacing, const int& margin) {
+	vector<PaperColumn> papers;
+	string bookString = prettyPrint(paragraphs, spacing);
+	vector<string> bookLines = Tokenize(bookString,'\n');
+	while (bookLines.size() % lines != 0) {
+		bookLines.push_back("");
+	}
+
+	int columns = 3;
+	int totalLines = 0;
+	double tmp = bookLines.size() / (double)lines;
+	int totalPages = (int) ceil((float) bookLines.size() / lines);
+	
+	for (int i = 0; i < totalPages; i++) {
+		string a = generatePaper(bookLines, lines, spacing, margin);
+		appendPaper(papers, a);
+	}
+	/*
+	for (auto n : papers) {
+		for (int i = 0; i < lines; i++) {
+			std::cout << n.returnLine(i) << std::endl;
+		}
+	}*/
+	printColumns(papers, columns, lines, spacing, margin);
 
 }
 
 int main(int argc, char* argv[])
 {
-	string filename = argv[2];
+	//string filename = argv[2];
+	string filename = "pg67698.txt";
 
 	ifstream file(filename);	//open file
 	string str((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
 	string s = removeDoubleNewline(str);	// remove \n after every line in project gutenburg books that makes it human readable
 	istringstream stream(s);
 	vector<string> paragraphs = separateParagraphs(stream);	// separate book into paragraphs
-
+	/*
 	if (string(argv[1]) == "prettyprint") {
 		int spacing = atoi(argv[3]);
 		string s = prettyPrint(paragraphs, spacing);
 		printString(s);
 	}
+	else if (string(argv[1]) == "twocolumn") {
+		int lines = atoi(argv[3]);
+		int spacing = atoi(argv[4]);
+		int margin = atoi(argv[5]);
+		twocolumn(paragraphs, lines, spacing, margin);
+	}
+	*/
 
+	int lines = 40;
+	int spacing = 38;
+	int margin = 4;
+	twocolumn(paragraphs, lines, spacing, margin);
 }
