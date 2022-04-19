2f0c861984e681dc5c44e922f8819dbc89672f7a
Samuel Gallagher <segallagher@alaska.edu>
2022-04-18 23:45:26 -0800

    changed something

--- TemplateReadWrite/myreadwrite.hpp
+++ TemplateReadWrite/myreadwrite.hpp
@@ -1,4 +1,7 @@
 #pragma once
+#ifndef MYREADWRITE_HPP
+#define MYREADWRITE_HPP
+
 
 #include <fstream>
 #include <vector>
@@ -13,3 +16,5 @@ template<typename T>
 void myWrite(std::ostream& ofile,const T &writeT) {
 	ofile.write(reinterpret_cast<const char*>(&writeT), sizeof(writeT));
 }
+
+#endif
