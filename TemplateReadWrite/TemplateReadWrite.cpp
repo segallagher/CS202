8e93e97973377916ffb1042bb721fbfd8657fc94
Samuel Gallagher <segallagher@alaska.edu>
2022-04-18 21:32:24 -0800

    made myRead(), myWrite() templates, made bad TemplateReadWrite.cpp testing code

--- TemplateReadWrite/TemplateReadWrite.cpp
+++ TemplateReadWrite/TemplateReadWrite.cpp
@@ -1,19 +1,42 @@
 #include <iostream>
 #include "myreadwrite.hpp"
+#include <vector>
 
 int main()
 {
-	std::ofstream ofile("test.txt", std::ios::out, std::ios::binary);
-	myWrite(ofile, 65);
-	myWrite(ofile, 42);
-	myWrite(ofile, 1);
-	myWrite(ofile, 77);
+	// Create file with doubles
+	std::vector<double> doubles{ 35,-62,60.9,1.0 };
+	std::ofstream ofile("doubles.txt", std::ios::out, std::ios::binary);
+	for (auto n : doubles) {
+		myWrite(ofile, n);
+	}
 	ofile.close();
-	std::ifstream ifile("test.txt", std::ios::in, std::ios::binary);
-	int a;
+
+	// Read file with doubles
+	std::ifstream ifile("doubles.txt", std::ios::in, std::ios::binary);
+	double a;
 	myRead(ifile, a);
 	while (!ifile.eof()) {
 		std::cout << a << std::endl;
 		myRead(ifile, a);
+	}
+
+	std::cout << "--------------------------" << std::endl;
+
+	// Create file with ints
+	std::vector<int> ints{ 1,-7,8,94 };
+	std::ofstream ofile2("ints.txt", std::ios::out, std::ios::binary);
+	for (auto n : ints) {
+		myWrite(ofile2, n);
+	}
+	ofile2.close();
+
+	// Read file with ints
+	std::ifstream ifile2("ints.txt", std::ios::in, std::ios::binary);
+	int b;
+	myRead(ifile2, b);
+	while (!ifile2.eof()) {
+		std::cout << b << std::endl;
+		myRead(ifile2, b);
 	}
 }
--- TemplateReadWrite/myreadwrite.hpp
+++ TemplateReadWrite/myreadwrite.hpp
@@ -4,10 +4,12 @@
 #include <vector>
 #include <string>
 
-void myRead(std::ifstream& ifile,int &readT) {
+template<typename T>
+void myRead(std::ifstream& ifile,T &readT) {
 	ifile.read(reinterpret_cast<char*>(&readT),sizeof(readT));
 }
 
-void myWrite(std::ostream& ofile,const int &writeT) {
+template<typename T>
+void myWrite(std::ostream& ofile,const T &writeT) {
 	ofile.write(reinterpret_cast<const char*>(&writeT), sizeof(writeT));
 }
