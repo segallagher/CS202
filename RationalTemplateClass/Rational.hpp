68ff268de7c00d6b41714de74c3f0134bd3481e3
Samuel Gallagher <segallagher@alaska.edu>
2022-04-18 23:44:57 -0800

    added skeleton or Rational class

---
+++ RationalTemplateClass/Rational.hpp
@@ -0,0 +1,25 @@
+#pragma once
+#ifndef RATIONAL_HPP
+#define RATIONAL_HPP
+
+class Rational
+{
+public:
+	Rational();
+	~Rational();
+
+private:
+
+};
+
+Rational::Rational()
+{
+}
+
+Rational::~Rational()
+{
+}
+
+
+#endif // !RATIONAL_HPP
+
